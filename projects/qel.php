d16c330f540d7666408727585bb68fb24ffe71da
brock a <brock.wright.anderson@gmail.com>
2013-01-31 22:48:17 -0600

    Update qel

--- projects/qel.php
+++ projects/qel.php
@@ -2,13 +2,26 @@
 
 require_once('../hf.php');
 
-printHeader('commercial work');
+printHeader('commercial work - QELink');
 
 ?>
 
+<a href="../work.php">&lt;&lt;&nbsp;Work</a>
+<br />
+<br />
+
 <h2> Quiet Electric Latch Link (QELink) </h2>
 
+<img src="qel_files/qel_board.png" alt="qel board" />
+<br />
+<br />
 Download the <a href="qel_files/manual.pdf">manual</a>.
+<br />
+See the <a  href="https://docs.google.com/presentation/d/1CwrpsmMsfAcG3fkkDfQVWaTw7IDbIkwQWdvo3Wv26lo/edit">presentation</a>.
+<br />
+Git the <a href="https://github.com/logic-fault/QELink_Server">central server</a>.
+<br />
+View the <a href="https://github.com/logic-fault/QELink">circuit and pcb KiCAD source</a> or just the <a href="qel_files/main_schem.pdf">schematic (PDF)</a>.
 
 <?
 
